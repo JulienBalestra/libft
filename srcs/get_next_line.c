93c6d5a6f93a993220cdddf53adfe02d100ce199
Julien Balestra <julien.balestra@gmail.com>
2015-11-21 13:46:34 +0100

    GetNextLine into makefile

--- srcs/get_next_line.c
+++ srcs/get_next_line.c
@@ -10,7 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
 #include "libft.h"
 #include <unistd.h>
 #include <stdlib.h>
